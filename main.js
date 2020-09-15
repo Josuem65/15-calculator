d2ce772714b4b1a621c513b908bd6138f1cc8dfb
Josue Martinez <josue.martinez65@yahoo.com>
2020-09-15 16:57:09 -0700

    update

---
+++ main.js
@@ -0,0 +1,30 @@
+const add = function (numOne, numTwo) {
+  const number1 = document.getElementById("num_one").textObject.value
+  const number2 = document.getElementById("num_two").textObject.value
+  const answer = number1 + number2
+  document.querySelector("#answer").innerHTML = answer
+}
+// document.querySelector('#answer').innerHTML = answer;
+// const subtract = function (num1, num2) {
+//   return num1 - num2
+// }
+
+// const multiply = function (number1, number2) {
+//   return number1 * number2
+// }
+
+// const divide = function (numero1, numero2) {
+//   return numero1 / numero2
+// }
+// const numberOne = document.getElementById("num_one").textObject.value
+// const numberTwo = document.getElementById("num_two").textObject.value
+
+// function getInputValue() {
+//   // Selecting the input element and get its value
+//   var inputVal = document.getElementById("num_one", "num_two").value
+//   console.log(inputVal)
+// }
+// #num_one, #num_two, #answer
+// #add, #subtract, #multiply, #divide
+// console.log(inputVal)
+// console.log(document.querySelector("#add").addEventListener("click", add)
